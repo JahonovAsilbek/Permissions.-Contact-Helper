712c29e4d4901e0a29a386b8ec794607417b1ac8
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-03-04 00:47:23 +0500

    permissions full need to import contact from device

--- app/src/main/java/uz/revolution/permissionscontacthelper/MainActivity.kt
+++ app/src/main/java/uz/revolution/permissionscontacthelper/MainActivity.kt
@@ -1,6 +1,7 @@
 package uz.revolution.permissionscontacthelper
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -9,64 +10,112 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import uz.revolution.permissionscontacthelper.adapter.ContactAdapter
 import uz.revolution.permissionscontacthelper.models.Contact
 
 class MainActivity : AppCompatActivity() {
+    private var adapter:ContactAdapter?=null
+    var requestCode=1
+    private var contacts:ArrayList<Contact>?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        loadData()
+        loadAdapters()
+        checkCallPermission()
+        onMessageClick()
+    }
 
-        val adapter = ContactAdapter()
-        val contacts = ArrayList<Contact>()
-        contacts.add(Contact("Olimjon Rustamov", "+998900123477"))
-        contacts.add(Contact("Jahonov Asilbek", "+998909920850"))
-        adapter.setAdapter(contacts)
-        contacts_rv.adapter = adapter
-
-        adapter.onCallClick = object : ContactAdapter.onAdapterCallClick {
-            override fun onCallClick(contact: Contact) {
-                if (ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.CALL_PHONE
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    ActivityCompat.requestPermissions(
-                        this@MainActivity,
-                        arrayOf(android.Manifest.permission.CALL_PHONE),
-                        1
-                    )
-                } else {
-                    makeCall(contact)
-                }
-            }
-        }
-
-        adapter.onMessageClick =
+    private fun onMessageClick() {
+        adapter?.onMessageClick =
             object : ContactAdapter.onAdapterMessageClick {
                 override fun onMessageClick(contact: Contact) {
-                    Snackbar.make(contacts_rv, "Shoshma, to'xta ....", Snackbar.LENGTH_LONG).show()
+                    val fragment = SendMessageFragment.newInstance(contact, "")
+                    supportFragmentManager.beginTransaction().addToBackStack("sms").add(R.id.container, fragment).commit()
                 }
-
             }
     }
 
-    fun makeCall(contact: Contact) {
-        val numberText = contact.phone
-        val intent = Intent(Intent.ACTION_CALL)
-        intent.data = Uri.parse("tel:$numberText")
-        if (ActivityCompat.checkSelfPermission(
+    private fun loadData() {
+        contacts = ArrayList()
+        contacts?.add(Contact("Olimjon Rustamov", "+998900123477"))
+        contacts?.add(Contact("Jahonov Asilbek", "+998909920850"))
+        contacts?.add(Contact("Shahzodbek", "+998332714061"))
+    }
+
+    private fun checkCallPermission() {
+        if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.CALL_PHONE
-            ) != PackageManager.PERMISSION_GRANTED
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
-            Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
-            return
+            makeCall()
+        } else {
+            requestSmsPermission()
         }
-        startActivity(intent)
+    }
+
+    private fun requestSmsPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)
+            && ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)){
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("Permission required")
+            dialog.setMessage("Permission to access the call is required")
+            dialog.setPositiveButton("OK"
+            ) { p0, p1 ->
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE,  Manifest.permission.READ_CONTACTS),requestCode)
+            }
+            val alertDialog=dialog.create()
+            alertDialog.show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE,  Manifest.permission.READ_CONTACTS),requestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == this.requestCode) {
+            if (grantResults.size == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED) {
+                makeCall()
+            } else {
+
+            }
+        }
+    }
+
+    private fun loadAdapters() {
+        adapter = ContactAdapter()
+        adapter?.setAdapter(contacts!!)
+        contacts_rv.adapter = adapter
+    }
+
+    fun makeCall() {
+        adapter?.onCallClick=object :ContactAdapter.onAdapterCallClick{
+            override fun onCallClick(contact: Contact) {
+                if (ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.CALL_PHONE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    val numberText = contact.phone
+                    val intent = Intent(Intent.ACTION_CALL)
+                    intent.data = Uri.parse("tel:$numberText")
+                    startActivity(intent)
+                } else {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.CALL_PHONE),requestCode)
+                    return
+                }
+            }
+
+        }
+
     }
 
 }
--- app/src/main/java/uz/revolution/permissionscontacthelper/SendMessageFragment.kt
+++ app/src/main/java/uz/revolution/permissionscontacthelper/SendMessageFragment.kt
@@ -1,13 +1,21 @@
 package uz.revolution.permissionscontacthelper
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.PendingIntent
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.SmsManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_send_message.view.*
 import uz.revolution.permissionscontacthelper.models.Contact
@@ -19,6 +27,7 @@ private const val ARG_PARAM2 = "param2"
 class SendMessageFragment : Fragment() {
 
     lateinit var root: View
+    var requestCode=1
     private var contact: Contact? = null
     private var param2: String? = null
 
@@ -35,24 +44,75 @@ class SendMessageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         root = inflater.inflate(R.layout.fragment_send_message, container, false)
-        sendMessage()
-
-
+        loadDataToView()
+        checkSmsPermission()
         return root
+    }
+
+    private fun checkSmsPermission() {
+        if (ContextCompat.checkSelfPermission(
+                root.context,
+                Manifest.permission.SEND_SMS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            sendMessage()
+        } else {
+            requestSmsPermission()
+        }
+    }
+
+    private fun requestSmsPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity as AppCompatActivity,Manifest.permission.SEND_SMS)){
+            val dialog = AlertDialog.Builder(root.context)
+            dialog.setTitle("Permission required")
+            dialog.setMessage("Permission to access the sms is required")
+            dialog.setPositiveButton("OK"
+            ) { p0, p1 ->
+                ActivityCompat.requestPermissions(activity as AppCompatActivity, arrayOf(Manifest.permission.SEND_SMS),requestCode)
+            }
+            val alertDialog=dialog.create()
+            alertDialog.show()
+        }else{
+            ActivityCompat.requestPermissions(activity as AppCompatActivity, arrayOf(Manifest.permission.SEND_SMS),requestCode)
+        }
+    }
+
+
+    private fun loadDataToView() {
+        root.name.text = contact?.name
+        root.phone.text=contact?.phone
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == this.requestCode) {
+            if (grantResults.size == 3 && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                sendMessage()
+            } else {
+            }
+        }
     }
 
     private fun sendMessage() {
         root.send_message.setOnClickListener {
-            val phone=contact?.phone
-            val message = root.message.text.toString()
+            if (ContextCompat.checkSelfPermission(root.context,Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED){
+                val phone = contact?.phone
+                val message = root.message.text.toString()
 
-            val intent = Intent(root.context, SendMessageFragment::class.java)
-            val pendingIntent = PendingIntent.getActivity(root.context, 0, intent, 0)
-            val sms:SmsManager= SmsManager.getDefault()
-            sms.sendTextMessage(phone, null, message, pendingIntent, null)
+                if (message.trim().isNotEmpty()) {
+                    val sms: SmsManager = SmsManager.getDefault()
+                    sms.sendTextMessage(phone, null, message, null, null)
+                    Snackbar.make(root, "Xabar jo'natildi", Snackbar.LENGTH_LONG).show()
+                    childFragmentManager.popBackStack()
+                } else {
+                    Snackbar.make(root,"Bo'sh xabar jo'natib bo'lmaydi",Snackbar.LENGTH_LONG).show()
+                }
 
-            Snackbar.make(root, "Xabar jo'natildi", Snackbar.LENGTH_LONG).show()
-            fragmentManager?.popBackStack()
+            }else{
+                checkSmsPermission()
+            }
         }
     }
 
@@ -66,4 +126,6 @@ class SendMessageFragment : Fragment() {
                 }
             }
     }
+
+
 }
